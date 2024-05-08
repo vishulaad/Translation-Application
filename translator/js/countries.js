0e2873b9ce0dab538bffea8585dede7ccb7c42a7
vishulaad <127011625+vishulaad@users.noreply.github.com>
2024-05-08 16:37:49 +0530

    Add files via upload

---
+++ translator/js/countries.js
@@ -0,0 +1,19 @@
+const countries = {
+    
+   
+    "bn-IN": "Bengali",
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "fr-FR": "French",
+    "gu-IN": "Gujarati",
+    "hi-IN": "Hindi",
+    "it-IT": "Italian",
+    "ja-JP": "Japanese",
+    "ko-KR": "Korean",
+    "ne-NP": "Nepali",
+    "ur-PK": "Pakistani",
+    "pa-IN": "Panjabi",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu",
+ 
+}
